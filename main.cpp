7dec3c4aba185b318cf354c9b82655d8e7d27e08
Protopovich Evgeny <64499132+kratos0-f@users.noreply.github.com>
2021-11-29 21:33:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by kratos0-f on 14.10.2021.
+//
+
+#include "lecture1_graphs_dfs/CycleSearch.h"
+
+int main() {
+
+    //используем наш класс, который будет искать цикл
+    CycleSearch cycleSearch;
+
+    //запустим решение
+    cycleSearch.solve();
+
+    return 0;
+}
